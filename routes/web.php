7bbc5227b086bfdd76b8edd089421c6d17b6924f
Jobayer Arman <jobayer.arman@gmail.com>
2017-10-23 03:17:29 +0600

    files scaffolding

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', 'PhotoController@index');
+Route::get('/about', function() {
+    return view('about');
+});
 
 Route::get('/work', function() {
 	return view('work');
